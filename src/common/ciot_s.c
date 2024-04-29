76f5801b09a86e902d980fbc49b961c2a97fe615
wesley-santos <wesleypro37@gmail.com>
2024-04-29 07:58:26 -0300

    refact: error code changed

--- src/common/ciot_s.c
+++ src/common/ciot_s.c
@@ -134,7 +134,7 @@ ciot_err_t ciot_s_process_byte(ciot_s_t self, uint8_t byte)
             {
                 self->idx = 0;
                 self->status = CIOT_S_STATUS_WAIT_START_DATA;
-                return CIOT_ERR_TERMINATOR_MISSING;
+                return CIOT_ERR_PROTOCOL_VIOLATION;
             }
             break;
     default:
